4e5768b96abf719d2ee6c5397c22f62353f234d7
THEWaterking <neamin.t.henok@gmail.com>
2026-06-30 17:14:03 -0500

    feat: implement separate flight profile and telemetry tracking classes

---
+++ FRQ (Unit 3)/Flight.java
@@ -0,0 +1,37 @@
+
+public class Flight {
+    private double mass;
+    private double velocity;
+    private double dragCo;
+
+    public Flight(double mass, double velocity, double dragCo){
+        this.mass = mass;
+        this.velocity = velocity;
+        this.dragCo= dragCo;
+    }
+
+    public double getVelocity(){
+        return velocity;
+    }
+
+    public double getMass(){
+        return mass;
+    }
+    public double getdragCo(){
+        return dragCo;
+    }
+    public double calcKin(){
+        return 0.5 * mass*Math.pow(velocity,2);
+    }
+    public void applyDrag(double airDensity){
+        //this.airDensity = airDensity;
+        double DF = 0.5 * dragCo *airDensity*Math.pow(velocity,2);
+        double deAlc = DF / mass;
+        velocity -= deAlc;
+        if (velocity <=0.0){
+            velocity = 0.0;
+        }
+        
+    }
+
+}
---
+++ FRQ (Unit 3)/Telemetry.java
@@ -0,0 +1,57 @@
+@SuppressWarnings("unused")
+public class Telemetry {
+    public static double maxEnergy;
+    private String hardwareID;
+    private Flight activeProf;
+    private boolean signalLost;
+
+    public Telemetry(String hardwareID, Flight object){
+        this.hardwareID = hardwareID;
+        this.activeProf = object;
+        signalLost = false;
+    }
+
+    public Flight getProfile(){
+        return activeProf;
+    }
+
+    public boolean isLost(){
+        if (signalLost){
+            return true;
+        }
+        return false;
+    }
+
+    public void processFlightStep(double localAirDensity){
+        if(signalLost){
+            return;
+        }
+        activeProf.applyDrag(localAirDensity);
+        double currentEng = activeProf.calcKin();
+        if(currentEng > maxEnergy){
+            maxEnergy = currentEng;
+        }
+
+        if ((activeProf.getVelocity() > 343) && (currentEng > 1000000)){
+            signalLost = true;
+        }
+    }
+}
+
+class Flight {
+    private double velocity;
+
+    public void applyDrag(double localAirDensity) {
+        // placeholder implementation
+    }
+
+    public double calcKin() {
+        return 0;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+}
+
+
